82d2f5b512cef1307d2a16f52ead96ca6f825f7b
breno morais <breno.lopes.morais@hotmail>
2018-07-24 15:53:31 -0300

    criando codec metodo encoder: obj > json

--- src/main/java/com/brenomorais/escola/models/Aluno.java
+++ src/main/java/com/brenomorais/escola/models/Aluno.java
@@ -4,79 +4,73 @@ import java.util.Date;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Aluno {
 	
 	private ObjectId id;
-	private String nome;
-	private Date dataNascimento;
-	private Curso curso;
-	private List<Nota> notas;
-	private List<Habilidade> habilidades;
 	
-	public void setId(ObjectId id) {
-		this.id = id;
-	}
-	public ObjectId getI() {
-		return id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Date getDataNascimento() {
-		return dataNascimento;
-	}
-	public void setDataNascimento(Date dataNascimento) {
-		this.dataNascimento = dataNascimento;
-	}
-	public Curso getCurso() {
-		return curso;
-	}
-	public void setCurso(Curso curso) {
-		this.curso = curso;
-	}
-	public List<Nota> getNotas() {
-		return notas;
-	}
-	public void setNotas(List<Nota> notas) {
-		this.notas = notas;
-	}
-	public List<Habilidade> getHabilidades() {
-		return habilidades;
-	}
-	public void setHabilidades(List<Habilidade> habilidades) {
-		this.habilidades = habilidades;
-	}
+	private String nome;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date dataNascimento;
+	
+	private Curso curso;
+	
+	private List<Nota> notas;
+	
+	private List<Habilidade> habilidades;
+
 	public ObjectId getId() {
 		return id;
 	}
-	
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Aluno other = (Aluno) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}	
 
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Curso getCurso() {
+		return curso;
+	}
+
+	public void setCurso(Curso curso) {
+		this.curso = curso;
+	}
+
+	public List<Nota> getNotas() {
+		return notas;
+	}
+
+	public void setNotas(List<Nota> notas) {
+		this.notas = notas;
+	}
+
+	public List<Habilidade> getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(List<Habilidade> habilidades) {
+		this.habilidades = habilidades;
+	}
+
+	public Aluno criarId() {
+		setId(new ObjectId());
+		return this;
+	}
 }
--- src/main/java/com/brenomorais/escola/repositories/AlunoRepository.java
+++ src/main/java/com/brenomorais/escola/repositories/AlunoRepository.java
@@ -1,23 +1,42 @@
 package com.brenomorais.escola.repositories;
 
+import org.bson.Document;
+import org.bson.codecs.Codec;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
 import org.springframework.stereotype.Repository;
 
+import com.brenomorais.escola.codec.AlunoCodec;
 import com.brenomorais.escola.models.Aluno;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-@Repository
-public class AlunoRepository {
-	
-	public void salvar(Aluno aluno) {
-		
-		MongoClient cliente = new MongoClient();
-		MongoDatabase bancoDeDados = cliente.getDatabase("test");
-		MongoCollection<Aluno> alunos = bancoDeDados.getCollection("alunos", Aluno.class);
-		
-		alunos.insertOne(aluno);
-		
-	}
 
+
+@Repository
+public class AlunoRepository {	
+
+	public void salvar(Aluno aluno){
+		
+		  Codec<Document> codec = MongoClient.getDefaultCodecRegistry().get(Document.class);
+
+		  AlunoCodec alunoCodec = new AlunoCodec(codec);
+
+		  CodecRegistry registro = CodecRegistries.fromRegistries(
+		      MongoClient.getDefaultCodecRegistry(), 
+		      CodecRegistries.fromCodecs(alunoCodec));
+
+		  MongoClientOptions options = MongoClientOptions.builder().codecRegistry(registro).build();
+
+		  MongoClient cliente = new MongoClient("localhost:27017", options);
+		  MongoDatabase bancoDeDados = cliente.getDatabase("test");
+		  MongoCollection<Aluno> alunos = bancoDeDados.getCollection("alunos", Aluno.class);
+		  
+		  alunos.insertOne(aluno);
+	  
+		  cliente.close();
+
+		}
 }

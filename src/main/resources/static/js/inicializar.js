b28bcdb29ddf05aaf9d24533d8b6c49a399c97a9
breno morais <breno.lopes.morais@hotmail>
2018-07-26 22:11:11 -0300

    ajustes front-end

--- src/main/resources/static/js/inicializar.js
+++ src/main/resources/static/js/inicializar.js
@@ -1,10 +1,24 @@
 $(function() {
 
 	$(document).ready(function() {
+		
 		$('.datepicker').pickadate({
-			selectMonths : true,
-			selectYears : 20
-		});
+		    monthsFull: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+		    monthsShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+		    weekdaysFull: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabádo'],
+		    weekdaysShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+		    today: 'Hoje',
+		    clear: 'Limpar',
+		    close: 'Pronto',
+		    labelMonthNext: 'Próximo mês',
+		    labelMonthPrev: 'Mês anterior',
+		    labelMonthSelect: 'Selecione um mês',
+		    labelYearSelect: 'Selecione um ano',
+		    selectMonths: true, 
+		    selectYears: 20,
+		    format: 'dd/mm/yyyy'
+		 });
+		
 		$('select').material_select();
 	});
 });
